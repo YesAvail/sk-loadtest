7828da97ebc5a1981f898898a79536917c53ec45
keshav Sharma <72795959+ksharma20@users.noreply.github.com>
2022-01-21 23:26:05 +0530

    Update locust-setup.sh
    Removed Make dir and git clone

--- locust-setup.sh
+++ locust-setup.sh
@@ -5,9 +5,6 @@ sudo apt install python3
 sudo apt install python3-pip
 sudo systemctl enable supervisor 
 sudo systemctl start supervisor
-mkdir code
-cd code/
-git clone https://github.com/YesAvail/sk-loadtest.git
 sudo mv locust.conf /etc/supervisor/conf.d/locust.conf
 sudo supervisorctl reread
 sudo supervisorctl update
